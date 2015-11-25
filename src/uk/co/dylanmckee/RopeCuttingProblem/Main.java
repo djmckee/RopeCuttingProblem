727509a4cc5d5ec6b81634c64d03a222e60f2216
Dylan McKee <dylan@djmckee.co.uk>
2015-11-25 16:17:47 +0000

    Init commit

---
+++ src/uk/co/dylanmckee/RopeCuttingProblem/Main.java
@@ -0,0 +1,8 @@
+package uk.co.dylanmckee.RopeCuttingProblem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/uk/co/dylanmckee/RopeCuttingProblem/Rope.java
@@ -0,0 +1,53 @@
+package uk.co.dylanmckee.RopeCuttingProblem;
+
+/**
+ * Purpose: A simple data structure class to represent a length of rope that is comparable and therefore can be sorted.
+ * Author: Dylan McKee
+ * Date: 25/11/2015
+ *
+ */
+public class Rope implements Comparable<Rope> {
+    // The length of the rope, in meters
+    private int length;
+
+    /**
+     * A method to get the length of the rope, in meters.
+     * @return the length of the rope, in meters
+     */
+    public int getLength() {
+        return this.length;
+    }
+
+    /**
+     * A method to set the new length of the rope - for example after cutting - in meters.
+     * @param length the new length of the rope, in meters.
+     */
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+
+    /**
+     * Compares this Rope instance with the Rope instance passed to it; compares by length in ascending order.
+     */
+    @Override
+    public int compareTo(Rope o) {
+        if (this.getLength() == o.getLength()) {
+            // They're equal
+            return 0;
+        } else if (this.getLength() > o.getLength()) {
+            // This rope is longer than the other
+        }
+        return 0;
+    }
+
+    /**
+     * Return a string describing the current rope instance (including its length)
+     * @return a human readable String object describing the current rope instance, including its length in meters.
+     */
+    @Override
+    public String toString() {
+        return "Rope - length: " + length + "m";
+    }
+
+}
