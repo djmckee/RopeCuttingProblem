6982e48e9864bccb40efd8f45edd78dddf212997
Dylan McKee <dylan@djmckee.co.uk>
2015-11-28 17:06:39 +0000

    package info

---
+++ src/uk/co/dylanmckee/RopeCuttingProblem/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Contains the classes required for the CSC2023 Rope Cutting Problem algorithm implementation.
+ * The main runnable class is Main, and the problem solving work itself is carried out in RopeCuttingProblem,
+ * the other classes are all either data structures that I created to hold rope/order information in, or JUnit unit
+ * test classes to test the solution implementation and the data structures (along with providing runtime analysis for
+ * the solution).
+ *
+ *
+ * @author Dylan McKee
+ * @version 1.0
+ *
+ */
+package uk.co.dylanmckee.RopeCuttingProblem;
